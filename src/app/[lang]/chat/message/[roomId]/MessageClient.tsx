3d5175e8e3682a6d58fbe961e9a04b7bb9b3b055
yuth <yuth_style88@hotmail.com>
2025-11-03 19:54:45 +0700

    add isHydrated

--- src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
+++ src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
@@ -6,26 +6,31 @@ import {RoomNotFound} from "@/components/RoomNotFound";
 import {useMyUser} from "@/hooks/api/profile/useMyUser";
 import {useRoomsStore} from "@/modules/chat/store/roomsStore";
 import {PhoenixChatBridgeProvider} from "@/modules/chat/contexts/PhoenixChatBridgeProvider";
+import LoadingBlur from "@/components/Common/Loading/LoadingBlur";
 
 export default function MessageClient({roomId}: { roomId: string }) {
-    const isLoggedIn = UserService.Instance.isLoggedIn;
-    const {localUser} = useMyUser();
-    const {getRoom, findPartner} = useRoomsStore();
-    const room = getRoom(roomId);
-    const partner = findPartner(roomId, localUser?.id);
+  const isLoggedIn = UserService.Instance.isLoggedIn;
+  const {localUser} = useMyUser();
+  const {getRoom, findPartner} = useRoomsStore();
+  const room = getRoom(roomId);
+  const partner = findPartner(roomId, localUser?.id);
+  const isHydrated = useRoomsStore((s) => s.isHydrated);
 
-    if (!room || !partner) {
-        return <RoomNotFound/>;
-    }
+  if(!isHydrated) {
+    return <LoadingBlur text="Preparing chat..."/>;
+  }
+  if(!room || !partner) {
+    return <RoomNotFound/>;
+  }
 
-    return (
-        <PhoenixChatBridgeProvider isLoggedIn={isLoggedIn} roomId={roomId}>
-            <ChatRoomView
-                post={room.post}
-                partner={partner}
-                roomData={room}
-                localUser={localUser!}
-            />
-        </PhoenixChatBridgeProvider>
-    );
+  return (
+    <PhoenixChatBridgeProvider isLoggedIn={isLoggedIn} roomId={roomId}>
+      <ChatRoomView
+        post={room.post}
+        partner={partner}
+        roomData={room}
+        localUser={localUser!}
+      />
+    </PhoenixChatBridgeProvider>
+  );
 }
