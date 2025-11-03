a26c6969737d65787f9cfccf48405369f9de4b31
yuth <yuth_style88@hotmail.com>
2025-11-03 18:12:03 +0700

    fix error

--- src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
+++ src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
@@ -6,12 +6,15 @@ import {RoomNotFound} from "@/components/RoomNotFound";
 import {useMyUser} from "@/hooks/api/profile/useMyUser";
 import {useRoomsStore} from "@/modules/chat/store/roomsStore";
 import {PhoenixChatBridgeProvider} from "@/modules/chat/contexts/PhoenixChatBridgeProvider";
+import {getIsoData} from "@/hooks/data/useIsoData";
+import {RoomView} from "@/modules/chat/types";
 
 export default function MessageClient({roomId}: { roomId: string }) {
     const isLoggedIn = UserService.Instance.isLoggedIn;
     const {localUser} = useMyUser();
     const {getRoom, findPartner} = useRoomsStore();
     const room = getRoom(roomId);
+    const roomData = room ? room : getIsoData()?.chatRooms?.rooms.find( room => room.room.id = roomId) as RoomView;
     const partner = findPartner(roomId, localUser?.id);
 
     if (!room || !partner) {
@@ -23,7 +26,7 @@ export default function MessageClient({roomId}: { roomId: string }) {
             <ChatRoomView
                 post={room.post}
                 partner={partner}
-                roomData={room}
+                roomData={roomData}
                 localUser={localUser!}
             />
         </PhoenixChatBridgeProvider>
