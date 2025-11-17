01d039230553cf631bd5786e4bb5d251b1a73b91
koey <thekhoi.k@gmail.com>
2025-11-17 19:25:15 +0700

    change header for account-setting page

--- src/app/[lang]/(profile)/account-setting/layout.tsx
+++ src/app/[lang]/(profile)/account-setting/layout.tsx
@@ -1,31 +1,48 @@
+"use client";
+
 import Header from "@/components/Header";
 import AccountSettingWrapper from "@/containers/AccountSettingWrapper";
-import SpHeader from "@/containers/SpHeader";
 import {LayoutProps} from "@/types/layout";
+import NavBar from "@/components/Home/NavBar";
+import MobileSidebar from "@/components/MobileSidebar";
+import React, {useState} from "react";
 
 export default function ProfileLayout({
-  children,
-}: LayoutProps) {
-  return (
-    <>
-      <div className="hidden sm:block">
-        <Header type="primary"/>
-      </div>
-      <div className="block sm:hidden">
-        <SpHeader showSearch={false}/>
-      </div>
+                                          children,
+                                      }: LayoutProps) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-      <section className="min-h-screen pt-[3rem] sm:pt-[4.5rem] bg-[#F6F7F8]">
-        <div className="grid-container-desktop-banner w-full pt-6 pb-16 px-4 sm:px-0 sm:py-8">
-          <div className="col-start-2 col-end-3">
-            <div className="grid-cols-1 sm:grid-cols-[286px_1fr] grid gap-6 items-start">
-              <AccountSettingWrapper/>
-              <div className="">{children}</div>
+    return (
+        <>
+            <div className="hidden sm:block">
+                <Header type="primary"/>
             </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+            <div className="block sm:hidden">
+                {/* Mobile Header */}
+                <div className="block sm:hidden fixed top-0 inset-x-0 z-[1000] bg-primary">
+                    <NavBar
+                        isSidebarOpen={isSidebarOpen}
+                        onToggleSidebar={() => setIsSidebarOpen(v => !v)}
+                        className="text-white"
+                    />
+                </div>
+                <MobileSidebar
+                    isOpen={isSidebarOpen}
+                    onClose={() => setIsSidebarOpen(false)}
+                />
+            </div>
+
+            <section className="min-h-screen pt-[3rem] sm:pt-[4.5rem] bg-[#F6F7F8]">
+                <div className="grid-container-desktop-banner w-full pt-6 pb-16 px-4 sm:px-0 sm:py-8">
+                    <div className="col-start-2 col-end-3">
+                        <div className="grid-cols-1 sm:grid-cols-[286px_1fr] grid gap-6 items-start">
+                            <AccountSettingWrapper/>
+                            <div className="">{children}</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
 
